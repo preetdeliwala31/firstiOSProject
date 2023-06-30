4380ad1aefab4dc52566172aa7c86c2225a2a320
preetdeliwala31 <preetdeliwala31@gmail.com>
2023-06-30 08:02:24 -0400

    First Project Done

--- video_player/ViewController.swift
+++ video_player/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -13,7 +15,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
 
+    private func playVideo(){
+        
+        guard let path = Bundle.main.path(forResource: "pexels-mart-production-7565438 (1080p)", ofType: "mp4") else {
+            
+            debugPrint("Video Not Found!!")
+            
+            return
+        }
+        
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerController = AVPlayerViewController()
+        playerController.player = player
+        present(playerController, animated: true) {
+            player.play()
+        }
+    }
 
 }
 
